305f039f7681da9d607c196a9e318b1e7f273962
lfilella97 <lfilella97@gmail.com>
2023-02-28 17:00:44 +0100

    Change BEM span class naming

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -6,66 +6,55 @@ interface RegisterFormProps {
 
 const RegisterForm = ({ submitForm }: RegisterFormProps): JSX.Element => {
   return (
-    <>
+    <form className="form" onSubmit={submitForm}>
       <h2 className="form__title">Register for the event</h2>
-      <form className="form" onSubmit={submitForm}>
-        <label className="form__label" htmlFor="email">
-          Email
-          <input
-            className="form_input"
-            type="email"
-            placeholder="Introduce your email"
-            id="email"
-            required
-          />
-          <span
-            className="form__email-error form__email-error--visible"
-            hidden={true}
-          >
-            Email is required
-          </span>
-        </label>
-        <label className="form__label" htmlFor="password">
-          Password
-          <input
-            className="form__input"
-            type="password"
-            placeholder="Introduce your password"
-            id="password"
-            required
-          />
-          <span
-            className="form__password-error form__password-error--visible"
-            hidden={true}
-          >
-            Password is required
-          </span>
-        </label>
-        <label className="form__label" htmlFor="image">
-          Image
-          <input
-            className="form__input"
-            type="file"
-            placeholder="Introduce an image"
-            id="image"
-            required
-          />
-          <span
-            className="form__image-error form__image-error--visible"
-            hidden={true}
-          >
-            Image is required
-          </span>
-        </label>
-        <Button text="Sing up" />
-        <span
-          className="form__general-error form__general-error--visible"
-          hidden={true}
-        >
-          Sorry, something went wrong
+      <label className="form__label" htmlFor="email">
+        Email
+        <input
+          className="form_input"
+          type="email"
+          placeholder="Introduce your email"
+          id="email"
+          name="email"
+          required
+        />
+        <span className="form__error form__error--visible" hidden>
+          Email is required
         </span>
-      </form>
-    </>
+      </label>
+      <label className="form__label" htmlFor="password">
+        Password
+        <input
+          className="form__input"
+          type="password"
+          placeholder="Introduce your password"
+          id="password"
+          name="password"
+          required
+        />
+        <span className="form__error form__error--visible" hidden>
+          Password is required
+        </span>
+      </label>
+      <label className="form__label" htmlFor="image">
+        Image
+        <input
+          className="form__input"
+          type="file"
+          placeholder="Introduce an image"
+          id="image"
+          name="image"
+          required
+        />
+        <span className="form__error form__error--visible" hidden>
+          Image is required
+        </span>
+      </label>
+      <Button text="Sing up" />
+      <span className="form__error form__error--visible" hidden>
+        Sorry, something went wrong
+      </span>
+    </form>
   );
 };
 
